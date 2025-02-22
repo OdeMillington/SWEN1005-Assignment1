d8be3d137183b2a0e9d310f6976813d08ed42496
Ode Millington <odemillington05@gmail.com>
2025-02-22 16:48:25 -0400

    Fixed Bug - Accordion Not Closing

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const generateCalendar = () => {
     const accordion = document.createElement("div");
     const dateContainer = document.createElement("div");
     const dateClass = document.createElement("div");
-    
+
     dateClass.classList.add("date");
     accordion.classList.add("accordion");
     dateContainer.classList.add("dateContainer");
@@ -20,6 +20,26 @@ const generateCalendar = () => {
     tmpDate.setDate(currentDate.getDate() + i);
 
     dateContainer.textContent = tmpDate.toDateString();
+
+    // Handles closing/opening accordion
+    dateContainer.onclick = () => {
+      const allAccordions = document.querySelectorAll(".accordion");
+
+      // Close all accordions first
+      allAccordions.forEach((unclickedAccordion) => {
+        if (unclickedAccordion !== accordion) {
+          unclickedAccordion.style.display = "none";
+        }
+      });
+
+      // Toggle only the clicked one
+      if (accordion.style.display === "none") {
+        accordion.style.display = "block";
+      } else {
+        accordion.style.display = "none";
+      }
+    };
+
     calendarArea.appendChild(dateClass);
     dateClass.appendChild(dateContainer);
     dateClass.appendChild(accordion);
