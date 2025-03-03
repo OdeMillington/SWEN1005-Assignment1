bfa0211461e26aebb289b156c517029565869b29
Ode Millington <odemillington05@gmail.com>
2025-03-02 22:10:58 -0400

    Added Priority Bubble Colours +  Other Changes

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 
   tasks = {}
   tempDate = ''
+  tempTaskList = []
 
   const formatDate = (date) => {
     day = String(date.getDate()).padStart(2, "0");
@@ -30,17 +31,46 @@
       const accordion = document.createElement("div");
       const dateContainer = document.createElement("div");
       const dateClass = document.createElement("div");
+      const taskList = document.createElement("ul");
 
       dateClass.classList.add("date");
       accordion.classList.add("accordion");
       dateContainer.classList.add("dateContainer");
+      taskList.classList.add("taskList");
 
       // Sets temporary date to update each date bar as the loop runs
       let tmpDate = new Date(currentDate);
       tmpDate.setDate(currentDate.getDate() + i);
 
-      dateContainer.textContent = tmpDate.toDateString();
+      monthNum = tmpDate.getMonth()
+      monthName = '';
 
+      switch (monthNum+1) {
+        case 1: monthName = "January"; break;
+        case 2: monthName = "February"; break;
+        case 3: monthName = "March"; break;
+        case 4: monthName = "April"; break;
+        case 5: monthName = "May"; break;
+        case 6: monthName = "June"; break;
+        case 7: monthName = "July"; break;
+        case 8: monthName = "August"; break;
+        case 9: monthName = "September"; break;
+        case 10: monthName = "October"; break;
+        case 11: monthName = "November"; break;
+        case 12: monthName = "December"; break;
+      }
+
+      dateContainer.textContent = `${monthName} ${tmpDate.getDate()}, ${tmpDate.getFullYear()}`
+
+      if (tasks[tempDate]) {
+        tasks[tempDate].task.forEach((task, index) => {
+          const taskItem = document.createElement("li");
+          taskItem.textContent = `${task} - ${tasks[tempDate].priority[index]}`;
+          taskList.appendChild(taskItem);
+        });
+      }
+  
+  
       // Handles closing/opening accordion
       dateContainer.onclick = () => {
         const allAccordions = document.querySelectorAll(".accordion");
@@ -53,18 +83,24 @@
 
         if (accordion.style.display === "none") {
           accordion.style.display = "block";
+          accordion.classList.add("accordionToggled")
+          dateContainer.classList.add("accordionToggledDate")
         } else {
           accordion.style.display = "none";
+          accordion.classList.remove("accordionToggled")
+          dateContainer.classList.remove("accordionToggledDate")
         }
       };
 
       const addTaskBtn = document.createElement("button");
       addTaskBtn.classList.add("addTaskBtn");
+      addTaskBtn.classList.add("addTaskBtn")
       addTaskBtn.innerHTML = "Add Task";
       addTaskBtn.addEventListener("click", () => {
         if (inputTaskBox.style.display == "none") {
           inputTaskBox.style.display = "block";
           tempDate = formatDate(tmpDate)
+          tempTaskList = taskList;
         } else {
           inputTaskBox.style.display = "none";
         }
@@ -74,15 +110,11 @@
       dateClass.appendChild(dateContainer);
       dateClass.appendChild(accordion);
       accordion.appendChild(addTaskBtn);
+      accordion.appendChild(taskList); 
     }
   };
 
   addTask.addEventListener("click", () => {
-
-    if (tempDate == '') {
-      return
-    }
-
 
     const taskTitleValue = document.querySelector("#taskTitle").value;
     const dueDateValue = document.querySelector("#dueDate").value;
@@ -105,6 +137,23 @@
     tasks[tempDate].priority.push(priorityValue);
     tasks[tempDate].dueDate.push(dueDateValue);
     tasks[tempDate].status.push(statusValue);
+
+  const taskItem = document.createElement("div");
+  taskItem.classList.add("taskItem")
+  const priorityBuble = document.createElement("div")
+  priorityBuble.classList.add("priorityBuble")
+
+  if (priorityValue == '1') {
+    priorityBuble.setAttribute("style", "background-color:#E63946")
+  } else if (priorityValue == "2") {
+    priorityBuble.setAttribute("style", "background-color:orange")
+  } else {
+    priorityBuble.setAttribute("style", "background-color:#008080")
+  }
+
+  taskItem.textContent = `${taskTitleValue}`;
+  tempTaskList.appendChild(taskItem);
+  taskItem.appendChild(priorityBuble)
     
   });
 
